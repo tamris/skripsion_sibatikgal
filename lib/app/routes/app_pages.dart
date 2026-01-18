6e1a3ea2935b3bbeacc691129faf8117ef22151e
tamris <113833655+tamris@users.noreply.github.com>
2026-01-18 13:58:40 +0700

    update page

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -24,7 +24,7 @@ part 'app_routes.dart';
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.LOGIN_PAGE;
+  static const INITIAL = Routes.ONBOARDING;
 
   static final routes = [
     GetPage(
